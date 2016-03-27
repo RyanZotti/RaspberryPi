fe4de01e9896b5724d518a74940a73153a601305
Ryan Zotti <ryanzotti@Ryans-MacBook-Pro.local>
2016-03-27 17:21:33 -0400

    measure distance via ultrasound

---
+++ range_sensor.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+
+# I got all code from here:
+# http://www.modmypi.com/blog/hc-sr04-ultrasonic-range-sensor-on-the-raspberry-pi
+
+GPIO.setmode(GPIO.BCM)
+TRIG = 23
+ECHO = 24
+print("Distance Measurement In Progress")
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+
+# Set output pin to low
+GPIO.output(TRIG, False)
+
+print("Waiting For Sensor To Settle")
+time.sleep(2)
+GPIO.output(TRIG, True)
+time.sleep(0.00001)
+GPIO.output(TRIG, False)
+
+while GPIO.input(ECHO)==0:
+    pulse_start = time.time()
+
+while GPIO.input(ECHO)==1:
+    pulse_end = time.time()
+
+pulse_duration = pulse_end - pulse_start
+distance = pulse_duration x 17150
+distance = round(distance, 2)
+print("Distance:",distance,"cm")
+GPIO.cleanup()
+
+
+
